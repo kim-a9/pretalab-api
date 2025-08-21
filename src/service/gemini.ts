a2e9de17a1756c44f53819862e985d8462d3ee92
kim-a9 <kimberlysantosalves@gmail.com>
2025-08-21 12:38:32 -0300

    feat: integração ia

---
+++ src/service/gemini.ts
@@ -0,0 +1,12 @@
+// import { GoogleGenAI } from "@google/genai";
+// import dotenv from "dotenv";
+
+// const ai = new GoogleGenAI({
+//     apiKey: process.env.GEMINI_API_KEY
+// });
+
+// export const generateText = async (prompt: string) => ai.models.generateContent({
+//         model: 'gemini-2.5-flash',
+//         contents: prompt,
+//     });
+// }
